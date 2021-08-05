7e01c6e62ade70910d92246bce82f0cc532bf4a5
Emin Kocan <152120161100@ogrenci.ogu.edu.tr>
2021-08-05 23:13:03 +0200

    Angular Basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { IProduct } from './models/product';
+import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Web Shop';
+export class AppComponent implements OnInit{
+  title = 'Taraba Web Shop';
+  products: IProduct[];
+
+  constructor(private http: HttpClient)  {
+  }
+  ngOnInit(): void {
+   this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) =>
+    {
+      this.products = response.data;
+    }, error => {
+     console.log(error);
+    });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
