4dfd881e998eae7c104feacfd7c4c8f52274d2e3
Emin Kocan <152120161100@ogrenci.ogu.edu.tr>
2021-08-13 23:52:35 +0200

    API - Identity

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,6 @@
 using API.Errors;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -13,6 +14,12 @@ namespace API.Controllers
             _context = context;
         }
 
+        [HttpGet("testauth")]
+        [Authorize]
+        public ActionResult<string> GetSecretText()
+        {
+            return "secret stuff";
+        }
         [HttpGet("notfound")]
         public ActionResult GetNotFoundRequest()
         {
@@ -43,5 +50,6 @@ namespace API.Controllers
         {
             return Ok();
         }
+        
     }
 }
